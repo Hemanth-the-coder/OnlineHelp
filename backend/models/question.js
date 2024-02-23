d5822864853f0d987c66c15bd326497e952da20d
Hemanth Modani <hemanthmodani2001@gmail.com>
2024-02-23 18:56:15 +0530

    Add more question routes

--- backend/models/question.js
+++ backend/models/question.js
@@ -1,22 +1,29 @@
 const mongoose = require("mongoose");
 console.log("Questions model page");
-const questionSchema = mongoose.Schema({
-    questionTitle : {
-        type : String,
-        required : true
-    },
-    questionDescription : {
-        type : String,
-        required : true
-    },
-    userId : {
-        type : String,
-        required : true
-    },
-    answers : {
-        type : Array,
-        default : []
-    }
-} , {timestamps : true} , {strict : false})
 
-module.exports = mongoose.model("Question" ,questionSchema);
+const questionSchema = mongoose.Schema({
+    questionTitle: {
+        type: String,
+        required: true
+    },
+    questionDescription: {
+        type: String,
+        required: true
+    },
+    userId: {
+        type: String,
+        required: true
+    },
+    answers: [{
+        username: {
+            type: String,
+            required: true
+        },
+        answer: {
+            type: String,
+            required: true
+        }
+    }]
+}, { timestamps: true });
+
+module.exports = mongoose.model('Question', questionSchema);
--- backend/routes/questionRoutes.js
+++ backend/routes/questionRoutes.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const Question = require("../models/question");
+const User = require("../models/user");
 
 //Create a question 
 router.post('/' , async (req , res)=> {
@@ -23,6 +24,29 @@ router.get('/' , async (req ,res)=> {
      }catch(err){
         res.status(500).json(err);
      }
+})
+
+
+//Get All Questions asked by the User.
+router.get("/profileQuestions/:username" , async (req, res) => {
+    try{
+        const user = await User.findOne({username : req.params.username});
+        const questions = await Question.find({userId : user._id});
+        res.status(200).json(questions)
+    }catch(err){
+        res.status(500).json(err);
+    }
+})
+
+
+//Get all the Questions answered by the user.
+router.get("/profileAnswers/:username" , async (req, res)=> {
+    try{
+        const questions = await Question.find({'answers.username' : req.params.username});
+        res.status(200).json(questions)
+    }catch(err){
+        res.status(500).json(err);
+    }
 })
 
 module.exports = router;
